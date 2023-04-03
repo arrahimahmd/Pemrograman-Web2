6c1c5f12ab484d70494f9e4804fee40139644afb
arrahimahmd <arrahimahamdia@gmail.com>
2023-04-03 19:27:19 +0700

    Upload Tugas praktikum 6

--- Praktikum6/kartu/list_kartu.php
+++ Praktikum6/kartu/list_kartu.php
@@ -1,18 +1,18 @@
 <?php 
     require_once '../dbkoneksi.php';
-   $sql = "SELECT * FROM vendor";
+   $sql = "SELECT * FROM kartu";
    $rs = $dbh->query($sql);
 ?>
 
-      <a class="btn btn-success" href="form_vendor.php" role="button">Create Vendor</a>
+      <a class="btn btn-success" href="form_kartu.php" role="button">Create Iuran</a>
         <table class="table table-hover table-bordered" width="100%" border="1" cellspacing="2" cellpadding="2">
             <thead>
                 <tr class="table-primary text-uppercase">
                     <th>No</th>
                     <th>Kode</th>
                     <th>Nama</th>
-                    <th>Kota</th>
-                    <th>Kontak</th>
+                    <th>Diskon</th>
+                    <th>Iuran</th>
                     <th>Action</th>
                 </tr>
             </thead>
@@ -23,14 +23,14 @@
                 ?>
                     <tr>
                         <td><?=$nomor?></td>
-                        <td><?=$row['nomor']?></td>
+                        <td><?=$row['kode']?></td>
                         <td><?=$row['nama']?></td>
-                        <td><?=$row['kota']?></td>
-                        <td><?=$row['kontak']?></td>
+                        <td><?=$row['diskon']?></td>
+                        <td><?=$row['iuran']?></td>
                         <td>
-<a class="btn btn-primary" href="view_vendor.php?id=<?=$row['id']?>">View</a>
-<a class="btn btn-warning" href="edit_vendor.php?idedit=<?=$row['id']?>">Edit</a>
-<a class="btn btn-danger" href="delete_vendor.php?iddel=<?=$row['id']?>"
+<a class="btn btn-primary" href="view_kartu.php?id=<?=$row['id']?>">View</a>
+<a class="btn btn-warning" href="edit_kartu.php?idedit=<?=$row['id']?>">Edit</a>
+<a class="btn btn-danger" href="delete_kartu.php?iddel=<?=$row['id']?>"
 onclick="if(!confirm('Anda Yakin Hapus Data Pelanggan <?=$row['nama']?>?')) {return false}"
 >Delete</a>
 </td>
